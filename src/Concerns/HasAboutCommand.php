cd01399e1ec6ce0d5d7ca827bd92cbfd96cbd6c0
ondrejnyklicek <ondrejnyklicek@icloud.com>
2024-12-01 12:50:25 +0100

    Added getting version from composer Small improvements

--- src/Concerns/HasAboutCommand.php
+++ src/Concerns/HasAboutCommand.php
@@ -4,23 +4,51 @@ namespace NyonCode\LaravelPackageBuilder\Concerns;
 
 use Closure;
 use Composer\InstalledVersions;
+use Composer\Json\JsonFile;
 use Illuminate\Foundation\Console\AboutCommand;
+use Seld\JsonLint\ParsingException;
 
 trait HasAboutCommand
 {
-    protected array $composerRootPackageData = [];
+    /**
+     * @var array<string>
+     */
+    private array $composerData = [];
+    public string $version = '';
+    private bool $isAboutable = false;
 
-    public function __construct()
+
+    /**
+     * Retrieves a specific value from the composer.json file by key name.
+     *
+     * @param string $keyName The key to retrieve from composer.json.
+     * @return string The value associated with the given key, or an empty string if not found.
+     * @throws ParsingException
+     */
+    private function getComposerValue(string $keyName): string
     {
-        $this->composerRootPackageData = InstalledVersions::getRootPackage();
+        $jsonFile = new JsonFile($this->path('/../composer.json'));
+        if ($jsonFile->exists()) {
+            $this->composerData = $jsonFile->read();
+        }
+
+        return $this->composerData[$keyName] ?? '';
     }
 
     /**
-     * Get the version of the package.
+     * Retrieves the version of the package.
      *
-     * @return string
+     * @return string The version of the package.
+     * @throws ParsingException
      */
-    abstract public function isVersion(): string;
+    public function getVersion(): string
+    {
+        return empty($this->version)
+            ? InstalledVersions::getPrettyVersion(
+                $this->getComposerValue('name')
+            )
+            : $this->version;
+    }
 
     /**
      * Returns an array with the version of the package for AboutCommand.
@@ -30,9 +58,7 @@ trait HasAboutCommand
     private function version(): array
     {
         return [
-            'Version' => fn() => !empty($this->isVersion())
-                ? $this->isVersion()
-                : $this->composerRootPackageData['pretty_version'],
+            'Version' => fn() => $this->getVersion(),
         ];
     }
 
@@ -53,7 +79,7 @@ trait HasAboutCommand
      */
     private function data(): array
     {
-        if (!empty($this->isVersion())) {
+        if (!empty($this->getVersion())) {
             return array_merge($this->version(), $this->aboutData());
         }
 
@@ -67,11 +93,40 @@ trait HasAboutCommand
      */
     public function bootAboutCommand(): void
     {
-        if (!empty($this->packager)) {
-            AboutCommand::add(
-                section: $this->packager->name,
-                data: $this->data()
-            );
+        if (!empty($this->name)) {
+            AboutCommand::add(section: $this->name, data: $this->data());
         }
+    }
+
+    /**
+     * Sets whether the package should include information in the AboutCommand.
+     *
+     * @param bool $value Whether the package should be "aboutable."
+     * @return static
+     */
+    public function hasAbout(bool $value = true): static
+    {
+        $this->isAboutable = $value;
+        return $this;
+    }
+
+    /**
+     * Sets the version of the package.
+     *
+     * @param string $version The version of the package.
+     * @return static
+     */
+    public function hasVersion(string $version): static
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAboutable(): bool
+    {
+        return $this->isAboutable;
     }
 }
