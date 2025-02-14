021d7efcb64462f0cd8000ca7f502bee4e2f41ad
Ondřej Nyklíček <60318239+ONyklicek@users.noreply.github.com>
2025-02-14 19:51:41 +0100

    Restructured project
    Split into traits for better clarity
    Added support for commands.
    
    - Add register components with namespace
    - Add new tests
    
    - Update README.md
    - Update phpDoc in class
    
    Signed-off-by: Ondřej Nyklíček <60318239+ONyklicek@users.noreply.github.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace NyonCode\LaravelPackageToolkit\Tests;
 
 
+use Illuminate\Foundation\Application;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function setUp(): void
@@ -10,6 +12,15 @@ class TestCase extends \Orchestra\Testbench\TestCase
         parent::setUp();
     }
 
+    /**
+     * Set up the environment.
+     *
+     * This method is called before each test.
+     *
+     * @param Application $app The application instance.
+     *
+     * @return void
+     */
     protected function getEnvironmentSetUp($app): void
     {
         $app['config']->set('database.default', 'sqlite');
